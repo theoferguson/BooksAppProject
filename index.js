4568b5b9593f9057e7433792482b0ad55168183d
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-23 13:17:36 -0400

    initial form basics. Add image as a normalizeData function element.

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+function renderForm() {
+    const inputForm = document.createElement("form")
+    inputForm.innerHTML =
+        '<label for="searchByCharName">Search for SW Characters</label> ' +
+        '<input id="searchByCharName" type="text" placeholder="Enter SW Character Name here"/> ' +
+        '<input id="imageInput" type="text" placeholder="Enter image URL here"/> ' +
+        '<input type="submit" />';
+    document.querySelector("header").append(inputForm).value
+
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // to be written
+        inputForm.reset();
+
+    })
+}
+
+
+
 function getCharsFromSwapi(){
     fetch('https://swapi.dev/api/people/1')
     .then((resp) => resp.json())
-    .then(swapiChar => normalizeData(swapiChar))
+    .then(swapiChar => normalizeData(swapiChar, "https://upload.wikimedia.org/wikipedia/en/9/9b/Luke_Skywalker.png"))
 }
 
-function normalizeData(eachChar){
+function normalizeData(eachChar, imageURL){
     let charObject = {
         name: eachChar.name,
         height: eachChar.height,
@@ -13,7 +33,8 @@ function normalizeData(eachChar){
         skinColor: eachChar.skin_color,
         eyeColor: eachChar.eye_color,
         birthYear: eachChar.birth_year,
-        gender: eachChar.gender
+        gender: eachChar.gender,
+        image: imageURL
 
     }
     console.log(charObject)
